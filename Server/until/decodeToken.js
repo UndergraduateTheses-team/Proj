4ba8edfdcc555e7a806c5b6419ce9703a64f20ce
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2025-03-10 23:26:15 +0700

    change jwt from sync to async

--- Server/until/decodeToken.js
+++ Server/until/decodeToken.js
@@ -3,8 +3,6 @@ import User from "../src/models/users.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    //get token from cookies
-
     const token = req.cookies.jwt;
 
     if (!token) {
@@ -13,29 +11,58 @@ const protectRoute = async (req, res, next) => {
         .json({ error: "Unauthorized - No token provider !" });
     }
 
-    //decrypt token (token need to decode, secret key sign when create token)
-    const decoded = jwt.verify(token, "GOITOILAGI");
-
+    // const decoded = await jwt.verify(token, "GOITOILAGI");
+    // Wrap jwt.verify in a Promise
+    const decoded = await new Promise((resolve, reject) => {
+      jwt.verify(token, "GOITOILAGI", (err, decodedToken) => {
+        if (err) {
+          reject(err);  // Reject with the error if verification fails
+        } else {
+          resolve(decodedToken);  // Resolve with the decoded token
+        }
+      });
+    });
     if (!decoded) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorized - No token provider !" });
-    }
-
-    //find user by id and remove password from result
+            return res
+              .status(401)
+              .json({ error: "Unauthorized - No token provider !" });
+          }
+      
     const user = await User.findById(decoded.userId).select("-password");
-
+      
     if (!user) {
       return res.status(404).json({ error: "User not found !" });
     }
-
-    //set user for request
     req.user = user;
     next();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: err });
-  }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: err });
+    }
 };
 
 export default protectRoute;
+
+
+
+
+
+//add await
+
+//     if (!decoded) {
+//       return res
+//         .status(401)
+//         .json({ error: "Unauthorized - No token provider !" });
+//     }
+
+//     const user = await User.findById(decoded.userId).select("-password");
+
+//     if (!user) {
+//       return res.status(404).json({ error: "User not found !" });
+//     }
+//     req.user = user;
+//     next();
+// } catch (err) {
+//   console.log(err);
+//   return res.status(500).json({ error: err });
+// }
